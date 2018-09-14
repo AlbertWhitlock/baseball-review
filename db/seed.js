148b3bd9ecfb45064f6385351fc062d4eac4881c
Albert <albertwhitlock4@gmail.com>
2018-09-14 11:31:48 -0400

    Added links to images

--- db/seed.js
+++ db/seed.js
@@ -4,7 +4,7 @@ db.baseball.insert([
     make: "Rawlings",
     model: "Quattro",
     image:
-      "https://www.rawlings.com/dw/image/v2/BBBJ_PRD/on/demandware.static/-/Sites-master-catalog/default/dwabbb5c74/hi-res/UT8Q34-1.jpg?sw=260&sh=260&sm=fit",
+      "https://lh3.googleusercontent.com/proxy/kdlJd3HECuMwv2sfhLrvgO_eWFSaXMKTiMB1PjNVWRS-p9pZfZ5aer4PFhNfYHTKpBKLWtS4EhHAL2KUXI6QzUj6rJjEYzAss0EA0nsFAYkJ8bcIv5jGVrn3eqV4GQZ7Lsq4KXYHd--PEPWsIpoExIFFnZ9gGXqB5h4nPaqqrNUxWYebEVC9XLZ0GE7kT_THqpC07sV-gM9Y_oLHOQ0=s500-pd-e365-rw-pc0xffffff",
     description:
       "The Quatro is a perfect fit for the middle of the order hitter who can handle a longer barrel. This bat offers everything: performance, flex, balance and distance. Our patented hinge system provides focused flex, helping hitters stay through the ball and generate incredible pop.",
     reviews: []
@@ -13,7 +13,8 @@ db.baseball.insert([
     item: "bat",
     make: "Easton",
     model: "Mako",
-    image: "One",
+    image:
+      "https://lh3.googleusercontent.com/proxy/s6bkVuMSlBLsf86vLOhYVkSIt6kozXy2he0fItla4jxMyMxJ7jwWviZmL79-oUXn8i0GeVFjo6gMZEGsn80K6d652yqBKEorDIe9n5yRVQeyeveKyVWTHMsaiUi4rsoiva_K9KqVOeEsmrH9zC5rBfv4CH0gDhtlSITA3-_Legj7vfLZYQFVxCshe6468S03Vufpr1BAYhF4eSn4DrY=s500-pd-e365-rw-pc0xffffff",
     description:
       "TCT Thermo Composite Technology is the key to the MAKO's industry-leading swing speed, massive sweet spot, and pure feel on contact. This innovative material comprises both the barrel and handle which are united by Easton's patented 2-PC ConneXion Technology.",
     reviews: []
@@ -22,7 +23,8 @@ db.baseball.insert([
     item: "bat",
     make: "Marucci",
     model: "CAT 7",
-    image: "One",
+    image:
+      "https://lh3.googleusercontent.com/proxy/sUKUJh8OxEfCwhZIZLYrjSHxAsT193hqJ1Ac7ufpGrHAme0VzLkq0JG45DfIQLfZA4ZuZx03rbYkKhoxSwJTonXqORuQBgDOTWj5iQKdlJVHfTXfvYrgVhkavXZTmDnVPNUvHKqxHjHHeM5z0wDkrDoz3CUe6NoyKHogKOd6bx1UIzMHkiI=s500-pd-e365-rw-pc0xffffff",
     description:
       "New and improved AZ4X Alloy that dramatically improves durability and strength. Additionally, the optimized barrel design is more responsive and offers a sweet spot that is twice as large in comparison to the CAT 6.",
     reviews: []
@@ -31,7 +33,8 @@ db.baseball.insert([
     item: "bat",
     make: "DeMarini",
     model: "Vodoo",
-    image: "One",
+    image:
+      "https://lh3.googleusercontent.com/proxy/fRQUKFYf7wV-HnAbwvEQDopcrQhe2zRSc9jj-lccczEVITM3LvxZXIElXDQnQYIm2y20aJ14mW5HBpJdf6zyEpca27Wdp1qKSXRxbikNtp7oWXb_XYKUhfHqgYonJFqSkJIyUyiMK7bw6uppUEBLFXtgX6rxqNpa9R5MQt4NlTKCIzGwpZgZ2LXm1I1-Zti0pgrZn6cO6shDfFsbtwuK=s500-pd-e365-rw-pc0xffffff",
     description:
       "With a two-piece, hybrid construction, the Voodoo delivers the traditional sound of alloy bats with the lightweight, smooth feel of composite bats. As the most versatile option in DeMarini's lineup, there's a reason it has become one of the all-time most popular baseball bats in the game.",
     reviews: []
@@ -40,7 +43,8 @@ db.baseball.insert([
     item: "glove",
     make: "Rawlings",
     model: "Heart of Hide",
-    image: "One",
+    image:
+      "https://lh3.googleusercontent.com/proxy/m3orRI_S24ko45WtxUcNhrcKmG_5MGXyhwZlpFrh2JkNa---M4l4kS62syV8cfA3zahq-lQn-P26F7SD4NZA2qIQ_Dta_PN0icLzGNyU73jbzQwr5-zg_cDR-1BYgBSOvUKBw-7_Cz_AWAwG1slayQshluC2OOxlS_k0fLIvIkHDEFSGyg=s500-pd-e365-rw-pc0xffffff",
     description:
       'The name "Heart of the Hide" says it all, as the leather that is used for each glove is cut from the center of the highest quality leather hides available.',
     reviews: []
@@ -49,7 +53,8 @@ db.baseball.insert([
     item: "glove",
     make: "Wilson",
     model: "A2K",
-    image: "One",
+    image:
+      "https://lh3.googleusercontent.com/proxy/2Dy_xKFQpezfjZOQbtUfamE79kbK2Qe0ZabYqrag9AHh7WLK0gOXQgJZJ59Hr5DNTywZOYy5DFILI7HKWZtMYEwlh8jpVUDguAiIah-E0njT9KZHA95DH2GvEOeASk0l_B4Sx0Weji-VBfwYSayYTDCbkEOZToeCOamw3dFbgrln6BN78K4yHnYpnQzaqNw-00kSYVMgTshNKn18yr_5=s500-pd-e365-rw-pc0xffffff",
     description:
       "Wilson A2K gloves are the premier patterns that last longer and look better due to the master craftsmanship of glove guru Shigeaki.",
     reviews: []
@@ -58,7 +63,8 @@ db.baseball.insert([
     item: "glove",
     make: "Nokana",
     model: "Alpha Select",
-    image: "One",
+    image:
+      "https://lh3.googleusercontent.com/proxy/zM3ezCqyyoyJEnNhukgl4aKZyWFE1yFQmi4Hen-Z7d7xo6a8n-4mIkexDD00J_ia6wSUhSb8iRNaN-hOzrHrEooPDkiB3P7FAGaraP0jwDdZk9Hp5MTGSMq8S1VMx58Y9Jhg-iIARv1KWpIj7N7luSrJfQcAVSEckaPSEFdQTia5Ci9vtg=s500-pd-e365-rw-pc0xffffff",
     description:
       "Constructed from top-of-the-line leathers, Stampede and Buffalo for ideal structure, weight, and very easy break-in. ",
     reviews: []
@@ -67,7 +73,8 @@ db.baseball.insert([
     item: "glove",
     make: "Akadema",
     model: "Pro Soft",
-    image: "One",
+    image:
+      "https://lh3.googleusercontent.com/proxy/urKMT6lSdv6M_ri_TKHM5iOUvKNYxX6UFz08CoiMdVzPbDniCYAjE_wYSwOWBhz9UiiQBx6vjUOtbygdHP2--wGGA9fCAkeEe3-x8Lvew5DrtpbAMnb3N9lY8pcSVaXw4oaoHkQezJC8ST2r2qhqlDyy3P7a4GJvwkQ3pbbVlCtt9auz4A=s500-pd-e365-rw-pc0xffffff",
     description:
       "This model features a new two-tone color scheme that is sure to impress. The Pro Soft Series is Akadema's exclusive glove for comfort and a game-ready feel. ",
     reviews: []
@@ -76,7 +83,8 @@ db.baseball.insert([
     item: "ball",
     make: "Rawlings",
     model: "ROLB1",
-    image: "One",
+    image:
+      "https://lh3.googleusercontent.com/proxy/IabrFRstmFxJajqXmPNP8aUx_oUdLuQxYE36Or3NvW1QiLIxSLbQABLWfV9u_kZKCXpLBx2MOGKX-SP_-eyLqCGyQ3zVdstPIWgfYiDViI5csP0cg6uPSMoIn-LETwpod-agAn2mHhh-ibch11vleHd43LgN9S0pofsVRh_dFtEbrZ9uluuXIjrSJiuqA55vLeMNEJdLCmKBjze67a8=s500-pd-e365-rw-pc0xffffff",
     description:
       "This baseball features a full grain leather cover for added durability with Rawlings raised seams for improved grip. This ball also has a composite cork and rubber center for optimal performance.",
     reviews: []
@@ -85,7 +93,8 @@ db.baseball.insert([
     item: "ball",
     make: "MacGregor",
     model: "76C",
-    image: "One",
+    image:
+      "https://lh3.googleusercontent.com/proxy/XE1lfGnz5MzIRYeGpGJBr5dUSMeac3fjpO-c55e0FNKTkSSC8Mh_inda1olQbJUqsBFgFQkneAh8OtaVAj3pjiqNgY2oIG2R0kuyopcArY7UN5t-CoIRCvXZx40eeJ-kNHn_svyP2sZSAgs5Ms0JXGehER--MLOFqaU0WvDTzskyHVKgv37dkM6yA2oSgsydFHQGGhBlIbv5wpKl5u8=s500-pd-e365-rw-pc0xffffff",
     description:
       "Red, raised stitching provides a traditional design and a solid grip as pitchers deliver curve balls",
     reviews: []
@@ -94,7 +103,8 @@ db.baseball.insert([
     item: "ball",
     make: "Rawlings",
     model: "RCAL",
-    image: "One",
+    image:
+      "https://lh3.googleusercontent.com/proxy/ditfImxsi4WEhFaHMqhQRP4rPUvS_EMmMcS_83b0XZjLHwVr3sxYssmbja7XGpATeVkg9b-Dxb673nsumisQJIAefbv7TQEPh6sZqizjyPwLB6FMNdEOneA4zXUtWSMPxkqRG5FL6UDuTIjQSfHcndgt60uIn3z7XSFkw8vheeD4Yhzu7ZJ7dPM_fQ220O9lNEANa66-M9qrDu0zhtk=s500-pd-e365-rw-pc0xffffff",
     description:
       "This baseball has Rawlings superstitch raised seams with full grain leather cover and cushioned cork center which make it perfect for the Cal Ripken Baseball League.",
     reviews: []
@@ -103,7 +113,8 @@ db.baseball.insert([
     item: "ball",
     make: "Wilson",
     model: "A1010",
-    image: "One",
+    image:
+      "https://lh3.googleusercontent.com/proxy/V2oRJykegBdXUYqLUVMgZlG9b55gdlU46zN8_y9F2kPNeS0usVymo8SRc5CDO9584KKgbAGzBX0hJgVBKhgStRd0YRWUEk2354u_FTF7SxcKLf5FBEA2F5yaSv6L_jMcaXllaFDXNHzxL4HVNHeY46v1_yekrLSWUaNkX4eimiTynBKRwWac-LyMsd2QMZirDBGdrWO0sYfHrMWqkBCW=s500-pd-e365-rw-pc0xffffff",
     description:
       "Model A1010 High raised seams Premium grade full grain leather cover Premium quality wool windings Red cushioned cork center.",
     reviews: []
@@ -112,7 +123,8 @@ db.baseball.insert([
     item: "helmet",
     make: "Rawlings",
     model: "Coolflo",
-    image: "One",
+    image:
+      "https://lh3.googleusercontent.com/proxy/-I1lXoKn1qlvts8Uf3aqi8nIAecb64H03W3vEJCXPK_ZSRF8_4IMo8kT1Nhi3m2e4ttg2IQIudFL6gTtItRylczfU_H7VUD-N34HqJEGIMKeeTHyEQVRqeEbplRZRHeTpNUCM65hU20-faJGP0HYQwjWfOkl0eyywClLi8XYOThzV26KVQ=s500-pd-e365-rw-pc0xffffff",
     description:
       "With COOLFLO ventilation, proven Heat Exchange™ technology, and a professional fit, players have the confidence to staycool at the plate and on the base paths.",
     reviews: []
@@ -121,7 +133,8 @@ db.baseball.insert([
     item: "helmet",
     make: "Rawlings",
     model: "Velo",
-    image: "One",
+    image:
+      "https://lh3.googleusercontent.com/proxy/1Dn30e6EXieiK4VWnjxXRHdyAVahh2syrp8hUU-UWzLpA26-8xFdp3KGZdnQ_fUWBctjs69MyQdeBZXvft4xuCAMR4pe_R6Ni8mhZreOqvirFWE4X3aNmIx2Y557r1yIfcTzN8eI1BJgeZDQYGHsrdoEm_oywDOVEKt0kiLcYl5E6WurKdO-wdl5I28dPGNdy5w4Dfn4Ym8xsswy-VIX=s500-pd-e365-rw-pc0xffffff",
     description:
       "The Velo™ series has been constructed with 16 individual vents for optimal air flow and circulation allowing its wearer to stay cool and dry.",
     reviews: []
@@ -130,7 +143,8 @@ db.baseball.insert([
     item: "helmet",
     make: "Easton",
     model: "Z5",
-    image: "One",
+    image:
+      "https://lh3.googleusercontent.com/proxy/UA02XQ9JX7VFJZDJW4kOxvocg73iqqkR-gGQ5m7nVJkQRmYJe7uf1D-1Keh9_t_U1LZp-7VoPQ3YHJEjL6MT8HolTvOejm2ddqshZAtEzzZXmJOcsBhJhDxttkMdnRg8-Q8t4rpxayDOZK4fz3cyvYB_z3pDI4mcdBKGxgGXuj3RNiA700G2pg_LgvbrAMqk21ha4L7GDLVybt--FD0=s500-pd-e365-rw-pc0xffffff",
     description:
       "ABS plastic provides an excellent protective shell against high impacts. Dual Density Foam, made up of high density shock absorbing foam and low density comfort foam.",
     reviews: []
@@ -139,7 +153,8 @@ db.baseball.insert([
     item: "helmet",
     make: "Demarini",
     model: "Paradox",
-    image: "One",
+    image:
+      "https://lh3.googleusercontent.com/proxy/xExY0v4Cgs1028lKkYkz0hbOwo2uAV3B40PXvx8fdkWxH8UK21vMPJD9ME9KQ9d5kyHxqFxDFQ4XONaB4DmMKswMXlL1-J_vBdQ96sqxIko-5DXKC8vMmzz6hTDh2zV5XVRbRtw2GNsp6lmCqqke30iTkMG5vjpPFPGedkpH062U_NMFhg=s500-pd-e365-rw-pc0xffffff",
     description:
       "This helmet has a profile any ball player will appreciate. The bill offers optimal sightlines and the padding creates a fit you'll love.",
     reviews: []
