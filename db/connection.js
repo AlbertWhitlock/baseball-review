adbe2cc8c449c7969afcf34e7f0b9ff6ff6a2881
Albert <albertwhitlock4@gmail.com>
2018-09-14 12:42:58 -0400

    Updated connection file to use MLAB

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
-// if (process.env.NODE_ENV == "production") {
-// mongoose.connect(process.env.MLAB_URL);
-// } else {
-mongoose.connect("mongodb://localhost/baseball");
-// }
+if (process.env.NODE_ENV == "production") {
+  mongoose.connect(process.env.MLAB_URL);
+} else {
+  mongoose.connect("mongodb://localhost/baseball");
+}
 
 mongoose.Promise = Promise;
 module.exports = mongoose;
